560a8fd120771a9ccedc7d90d893f0a8628e72ee
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-03-26 18:47:50 +0100

    Add combinatoric syntax variation

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -1,9 +1,5 @@
 use rand::Rng;
 
-pub fn square(n: u32) -> u32 {
-	n * n
-}
-
 pub struct Question {
 	pub prompt: String,
 	pub answer: String,
@@ -12,12 +8,19 @@ pub struct Question {
 pub fn combinatoric() -> Question {
 	let n = rand::thread_rng().gen_range(2..10);
 	let r = rand::thread_rng().gen_range(1..n);
-	let prompt = format!("{{}}^{{{n}}}C_{{{r}}}", n = n, r = r);
+	let prompt = combinatoric_prompt(n, r);
 	let answer = factorial(n) / (factorial(r) * factorial(n - r));
 	return Question {
 		prompt,
 		answer: answer.to_string(),
 	};
+}
+fn combinatoric_prompt(n: u64, r: u64) -> String {
+	if rand::random() {
+		format!("{{}}^{{{n}}}C_{{{r}}}")
+	} else {
+		format!("{n} \\choose {r}")
+	}
 }
 
 fn factorial(n: u64) -> u64 {
